e5706aff1e153fb715065cfecc4e9447f53dc1b4
luzhaopan <745003249@qq.com>
2023-10-27 17:52:59 +0800

    fix: 验证修改

--- src/views/verify/sliderVerify.vue
+++ src/views/verify/sliderVerify.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="box">
-    <div id="slider" class="slider" :style="{ left: left + 'px' }"></div>
+    <div
+      id="slider"
+      class="slider"
+      :style="{ left: left + 'px' }"
+      @mousedown="onMouseDown"
+    ></div>
     <p class="text">{{ text }}</p>
     <div class="chute" :style="{ width: left + 'px' }"></div>
   </div>
@@ -10,32 +15,44 @@
   const text = ref('请向右滑动滑块')
   const left = ref(0)
   const flag = ref(false)
+  const isDragging = ref(false) // 是否正在拖动滑块
+  const dragStartX = ref(0) // 开始拖动时鼠标的x坐标 // 鼠标相对于滑块左边缘的偏移量
 
-  onMounted(() => {
-    const box: any = document.querySelector('.box')
-    const slider: any = document.querySelector('.slider')
-    slider.onmousedown = function (e) {
-      let downx = e.clientX
-      slider.onmousemove = function (e) {
-        const movex = e.clientX - downx
+  const onMouseDown = (e) => {
+    isDragging.value = true
+    dragStartX.value = e.clientX
+  }
 
-        if (movex > 0) {
-          left.value = movex
-          if (movex >= box.offsetWidth - slider.offsetWidth) {
-            text.value = '验证成功'
-            flag.value = true
-            slider.onmousemove = null
-            slider.onmousedown = null
-          }
-        }
+  const onMouseMove = (e) => {
+    if (!isDragging.value) return
+    const distance = e.clientX - dragStartX.value
+    if (distance > 0) {
+      left.value = distance
+      const box: any = document.querySelector('.box')
+      const slider: any = document.querySelector('.slider')
+      if (distance >= box.offsetWidth - slider.offsetWidth) {
+        text.value = '验证成功'
+        flag.value = true
+        window.removeEventListener('mousemove', onMouseMove)
+        // window.removeEventListener('mouseup', onMouseUp)
       }
     }
+  }
 
-    slider.onmouseup = function () {
-      slider.onmousemove = null
-      if (flag.value) return
-      left.value = 0
-    }
+  const onMouseUp = (e) => {
+    if (flag.value) return
+    isDragging.value = false
+    left.value = 0
+  }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', onMouseMove)
+    window.addEventListener('mouseup', onMouseUp)
+  })
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('mousemove', onMouseMove)
+    window.removeEventListener('mouseup', onMouseUp)
   })
 </script>
 
