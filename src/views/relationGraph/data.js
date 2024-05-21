c1186f5fcbbbd2a81ae9384994df8c318fce31a8
luzhaopan <745003249@qq.com>
2024-05-21 17:04:09 +0800

    fix

--- src/views/relationGraph/data.js
+++ src/views/relationGraph/data.js
@@ -1,4 +1,195 @@
-const nodes = [
+const nodes1 = [
+  {
+    id: 'a',
+    text: 'dim_trade_info1',
+    color: '#008080',
+    data: {
+      uuid: '1120023', // 表id,
+      fieldNames: [], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'num1', //  表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 0 // 0 不可裁剪 1 可裁剪
+    }
+  },
+  {
+    id: 'b',
+    text: 'dim_trade_info2',
+    color: '#008080',
+    data: {
+      uuid: '119172', // 表id,
+      fieldNames: [], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'num2', //  表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 0 // 0 不可裁剪 1 可裁剪
+    }
+  },
+  {
+    id: 'c',
+    text: 'dim_trade_info1',
+    color: '#ff8c00',
+    data: {
+      uuid: '112312', // 表id,
+      fieldNames: [], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'sales_num', //  表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 0 // 0 不可裁剪 1 可裁剪
+    }
+  },
+  {
+    id: 'd',
+    text: 'dim_trade_info1',
+    color: '#ffff80',
+    data: {
+      uuid: '11y322', // 表id,
+      fieldNames: [], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'amount', //  表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 1 // 0 不可裁剪 1 可裁剪
+    }
+  },
+  {
+    id: 'e',
+    text: 'dim_trade_info2',
+    color: '#ffff80',
+    data: {
+      uuid: '1134322', // 表id,
+      fieldNames: [], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'amount', // 表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 1 // 0 不可裁剪 1 可裁剪
+    }
+  },
+  {
+    id: 'f',
+    text: 'dim_trade_info3',
+    color: '#ffff80',
+    data: {
+      uuid: '11233', // 表id,
+      fieldNames: [], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'amount', //  表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 0 // 0 不可裁剪 1 可裁剪
+    }
+  },
+  {
+    id: 'g',
+    text: 'dim_trade_info4',
+    color: '#ffff80',
+    data: {
+      uuid: '112', // 表id,
+      fieldNames: [], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'amount', //  表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 0 // 0 不可裁剪 1 可裁剪
+    }
+  }
+]
+const lines1 = [
+  {
+    from: 'a',
+    to: 'c',
+    text: 'num1+num2',
+    fontColor: '#333',
+    data: {
+      lieageType: 0, // 0投影, 1 关联, 2 过滤
+      sqlId: 1 // [血缘sql id]
+    }
+  },
+  {
+    from: 'b',
+    to: 'c',
+    text: 'num1+num2',
+    fontColor: '#333',
+    data: {
+      lieageType: 1, // 0投影, 1 关联, 2 过滤
+      sqlId: 1 // [血缘sql id]
+    }
+  },
+  {
+    from: 'c',
+    to: 'd',
+    text: 'amount',
+    fontColor: '#333',
+    data: {
+      lieageType: 2, // 0投影, 1 关联, 2 过滤
+      sqlId: 1 // [血缘sql id]
+    }
+  },
+  {
+    from: 'c',
+    to: 'e',
+    text: 'amount',
+    fontColor: '#333',
+    data: {
+      lieageType: 0, // 0投影, 1 关联, 2 过滤
+      sqlId: 0 // [血缘sql id]
+    }
+  },
+  {
+    from: 'd',
+    to: 'f',
+    text: 'amount',
+    fontColor: '#333',
+    data: {
+      lieageType: 0, // 0投影, 1 关联, 2 过滤
+      sqlId: 0 // [血缘sql id]
+    }
+  },
+  {
+    from: 'e',
+    to: 'g',
+    text: 'amount',
+    fontColor: '#333',
+    data: {
+      lieageType: 0, // 0投影, 1 关联, 2 过滤
+      sqlId: 3 // [血缘sql id]
+    }
+  }
+]
+
+const nodes2 = [
   {
     id: 'a',
     text: 'dim_trade_info1',
@@ -100,7 +291,22 @@ const nodes = [
     text: 'dim_trade_info4',
     color: '#ffff80',
     data: {
-      // uuid: '', // 表id, fieldNames:[], //[包含字段的名称],
+      isExpand: false,
+      tabName: 'amount', //  表名
+      schema: 'scheme', // 所属scheme
+      tabType: 0, //表类型 0： 物理表， 1： 临时表， 2： 虚拟表， 3： 文件表
+      dbName: '', // 数据库名称
+      ownSystem: '', // 所属系统
+      fieldName: '', // 字段名称
+      nodeType: 0, // 0基准节点 1 下游 - 1 上游节点
+      croppingFlag: 0 // 0 不可裁剪 1 可裁剪
+    }
+  },
+  {
+    id: 'h',
+    text: 'dim_trade_info0',
+    color: '#ffff80',
+    data: {
       isExpand: false,
       tabName: 'amount', //  表名
       schema: 'scheme', // 所属scheme
@@ -113,7 +319,7 @@ const nodes = [
     }
   }
 ]
-const lines = [
+const lines2 = [
   {
     from: 'a',
     to: 'c',
@@ -173,7 +379,27 @@ const lines = [
       lieageType: 0, // 0投影, 1 关联, 2 过滤
       sqlId: 3 // [血缘sql id]
     }
+  },
+  {
+    from: 'e',
+    to: 'f',
+    text: 'amount3',
+    fontColor: '#333',
+    data: {
+      lieageType: 0, // 0投影, 1 关联, 2 过滤
+      sqlId: 3 // [血缘sql id]
+    }
+  },
+  {
+    from: 'c',
+    to: 'h',
+    text: 'amount3',
+    fontColor: '#333',
+    data: {
+      lieageType: 0, // 0投影, 1 关联, 2 过滤
+      sqlId: 3 // [血缘sql id]
+    }
   }
 ]
 
-export { nodes, lines }
+export { nodes1, lines1, nodes2, lines2 }
--- src/views/relationGraph/tableRelation.vue
+++ src/views/relationGraph/tableRelation.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <div class="toolbar">
-      <el-input v-model="searchText" placeholder="搜索" style="width: 200px" />
+      <el-input
+        v-model="searchParams.searchName"
+        placeholder="搜索"
+        style="width: 200px"
+      />
       <div class="toolbar-switch">
         <el-switch
           v-model="tableType"
@@ -28,7 +32,7 @@
         <span>上游层级：</span>
         <el-select
           style="width: 80px"
-          v-model="selectedUplevel"
+          v-model="commonParams.upstreamNum"
           placeholder="请选择"
         >
           <el-option
@@ -43,7 +47,7 @@
         <span>下游层级：</span>
         <el-select
           style="width: 80px"
-          v-model="selectedDownlevel"
+          v-model="commonParams.downstreamNum"
           placeholder="请选择"
         >
           <el-option
@@ -54,11 +58,9 @@
           ></el-option>
         </el-select>
       </div>
-      <el-radio-group v-model="selectedRadio" class="ml-4">
-        <el-radio label="json">投影</el-radio>
-        <el-radio label="form">过滤</el-radio>
-        <el-radio label="file">关联</el-radio>
-      </el-radio-group>
+      <el-checkbox v-model="commonParams.projectionType" label="投影" />
+      <el-checkbox v-model="commonParams.associationType" label="过滤" />
+      <el-checkbox v-model="commonParams.filterType" label="关联" />
       <el-button type="primary" @click="loadData">加载血缘</el-button>
     </div>
     <el-divider border-color="#dcdcdc" border-style="dashed" />
@@ -85,34 +87,34 @@
               <el-icon
                 v-if="node.data.croppingFlag"
                 @click.stop="toggleCropping(node)"
-                ><Scissor
-              /></el-icon>
+              >
+                <Scissor />
+              </el-icon>
             </div>
-            <div
-              v-if="isExpandAll"
-              :class="{
-                'table-content': true,
-                'exp-hight': node.data.isExpand,
-                'hide-hight': !node.data.isExpand
-              }"
-            >
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div>{{ node.data.tabName }}</div>
-              <div class="btn" @click.stop="toggleExpand(node)">
-                <span v-if="!node.data.isExpand">展开</span>
-                <span v-else>收起</span>
+            <div v-if="node.data.isExpand" class="table-content">
+              <el-scrollbar height="350px">
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+                <div>{{ node.data.tabName }}</div>
+              </el-scrollbar>
+            </div>
+            <div class="btn" @click="toggleExpand(node)">
+              <div v-if="!node.data.isExpand">
+                展开 <el-icon><ArrowDown /></el-icon>
+              </div>
+              <div v-else>
+                收起 <el-icon><ArrowUp /></el-icon>
               </div>
             </div>
           </div>
@@ -136,7 +138,7 @@
 </template>
 
 <script lang="ts" setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, reactive, onMounted } from 'vue'
   import RelationGraph, {
     RGJsonData,
     RGOptions,
@@ -146,18 +148,31 @@
     RGUserEvent,
     RelationGraphComponent
   } from 'relation-graph-vue3'
-  import { nodes, lines } from './data.js'
+  import { ElMessage, ElMessageBox } from 'element-plus'
+  import { nodes1, lines1, nodes2, lines2 } from './data.js'
+
+  const searchParams = reactive({
+    searchType: 1, // 1:表 2:字段
+    searchName: '搜索名称'
+  })
+
+  const commonParams = reactive({
+    showType: 1, // 1 穿透层 2 原始层
+    upstreamNum: 3, // 上游层级数
+    downstreamNum: 3, // 下游层级数
+    projectionType: true, // 是否选择投影 1是
+    associationType: false, // 是否选择关联关系	0否1是
+    filterType: false, // 是否选择过滤关系	0否1是
+    uuid: '22' // 基准节点表id
+  })
 
   const loading = ref(false)
   const isExpandAll = ref(false)
-  const searchText = ref('')
   const tableType = ref(false)
   const nodeType = ref(false)
-  const selectedUplevel = ref(3)
-  const selectedDownlevel = ref(3)
-  const selectedRadio = ref('json')
+  const graphInstance = ref()
 
-  const options = [
+  const options: any[] = [
     {
       label: 1,
       value: 1
@@ -180,8 +195,15 @@
     }
   ]
 
+  const graphData = reactive<any>({
+    rootId: 'c',
+    nodes: [],
+    lines: []
+  })
+
   const graphRef = ref<RelationGraphComponent | null>(null)
   const graphOptions: RGOptions = {
+    debug: false,
     layouts: [
       {
         label: 'Center',
@@ -197,6 +219,7 @@
     disableDragNode: true, // 是否禁用图谱中节点的拖动
     defaultNodeShape: 1, // 默认的节点形状，0:圆形；1:矩形
     defaultNodeWidth: 300,
+    // defaultNodeHeight: 100,
     defaultNodeBorderWidth: 0, // 默认的节点边框粗细（像素）
     defaultNodeBorderColor: '#333', // 默认的节点边框颜色
     defaultNodeFontColor: '#333', // 默认的节点文字颜色
@@ -227,50 +250,42 @@
 
   // 表类型切换
   const tableTypeSwitch = async (val) => {
-    console.log(val)
+    searchParams.searchType = val ? 2 : 1
+    console.log(val, searchParams.searchType)
     loading.value = true
-    // const graphInstance = graphRef.value?.getInstance()
     // graphInstance.options.defaultShowLineLabel = tableType.value
-
-    // await graphInstance.refresh()
     // graphInstance.zoomToFit()
+    graphData.rootId = 'c'
+    graphData.nodes = val ? nodes2 : nodes1
+    graphData.lines = val ? lines2 : lines1
     graphOptions.defaultShowLineLabel = tableType.value
-    const graphInstance = graphRef.value!.getInstance()
-    graphInstance.setOptions(graphOptions)
-    await graphInstance.refresh()
+    graphInstance.value.setOptions(graphOptions)
+    // await graphInstance.value.refresh();
+    await showGraph()
     loading.value = false
-    // await showGraph()
-    // nextTick(() => {
-    //   const graphInstance = graphRef.value!.getInstance()
-    //   graphInstance.setOptions(graphOptions)
-    //   graphInstance.refresh()
-    //   // showGraph()
-    //   // graphInstance.focusRootNode()
-    //   // graphInstance.setZoom(54)
-    // })
   }
 
   // 节点类型切换
   const nodeTypeSwitch = (val) => {
     console.log(val)
+    commonParams.showType = val ? 2 : 1
     // graphInstance.dataUpdated();
   }
 
   const loadData = () => {
-    console.log('loadData')
+    console.log('loadData', commonParams)
   }
 
+  // 关系图初始化
   const showGraph = async () => {
     const __graph_json_data: RGJsonData = {
-      rootId: 'c',
-      nodes: nodes,
-      lines: lines
+      ...graphData
     }
-    const graphInstance = graphRef.value?.getInstance()
-    if (graphInstance) {
-      await graphInstance.setJsonData(__graph_json_data)
-      await graphInstance.moveToCenter()
-      await graphInstance.zoomToFit()
+    graphInstance.value = graphRef.value!.getInstance()
+    if (graphInstance.value) {
+      await graphInstance.value.setJsonData(__graph_json_data)
+      await graphInstance.value.moveToCenter()
+      await graphInstance.value.zoomToFit()
     }
   }
 
@@ -279,9 +294,8 @@
     // const allChildIds = getAllConnectIds(nodeObject)
     const allChildIds = deepGetAllChildIds(nodeObject)
 
-    const graphInstance = graphRef.value?.getInstance()
-    if (graphInstance) {
-      for (const node of graphInstance.getNodes()) {
+    if (graphInstance.value) {
+      for (const node of graphInstance.value.getNodes()) {
         if (allChildIds.includes(node.id)) {
           node.opacity = 1
           // node.color = 'rgb(116,2,173)'
@@ -289,10 +303,11 @@
           // node.borderColor = 'yellow'
         } else {
           node.opacity = 0.1
+
           // node.color = nodeObject.color
         }
       }
-      for (const link of graphInstance.getLinks()) {
+      for (const link of graphInstance.value.getLinks()) {
         if (
           allChildIds.includes(link.fromNode.id) &&
           allChildIds.includes(link.toNode.id)
@@ -316,19 +331,21 @@
   }
 
   // 过去关联节点id
-  const getAllConnectIds = (node: RGNode, ids: string[] = []): string[] => {
-    ids.push(node.id)
-    ids.push(node.lot?.parent?.id)
-    for (const cNode of node.lot.childs) {
-      ids.push(cNode.id)
-    }
-    return ids
-  }
+  // const getAllConnectIds = (node: RGNode, ids: string[] = []): string[] => {
+  //   ids.push(node.id);
+  //   ids.push(node.lot?.parent?.id);
+  //   for (const cNode of node.lot.childs) {
+  //     ids.push(cNode.id);
+  //   }
+  //   return ids;
+  // };
 
   const deepGetAllChildIds = (node: RGNode, ids: string[] = []): string[] => {
     if (ids.includes(node.id)) return ids
     ids.push(node.id)
-    ids.push(node.lot?.parent?.id)
+    if (node.lot?.parent?.id) {
+      ids.push(node.lot?.parent?.id)
+    }
     for (const cNode of node.lot.childs) {
       deepGetAllChildIds(cNode, ids)
     }
@@ -336,13 +353,12 @@
   }
 
   const onCanvasClick = ($event?: RGUserEvent) => {
-    const graphInstance = graphRef.value?.getInstance()
-    if (graphInstance) {
-      for (const node of graphInstance.getNodes()) {
+    if (graphInstance.value) {
+      for (const node of graphInstance.value.getNodes()) {
         node.opacity = 1
         // node.color = undefined
       }
-      for (const link of graphInstance.getLinks()) {
+      for (const link of graphInstance.value.getLinks()) {
         link.relations.forEach((line) => {
           line.color = undefined
           line.animation = undefined
@@ -364,33 +380,93 @@
   // 展开所有节点
   const toggleExpandAll = () => {
     isExpandAll.value = !isExpandAll.value
+    if (graphInstance.value) {
+      for (const node of graphInstance.value.getNodes()) {
+        node.data.isExpand = isExpandAll.value
+      }
+    }
     onCanvasClick()
-    // const graphInstance = graphRef.value?.getInstance()
-    // if (graphInstance) {
-    //   for (const node of graphInstance.getNodes()) {
-    //     node.data.isExpand = !node.data.isExpand
-    //   }
-    // }
   }
 
   // 展开节点
   const toggleExpand = (node) => {
     node.data.isExpand = !node.data.isExpand
     // const graphInstance = graphRef.value!.getInstance()
-    nextTick(() => {
-      // graphInstance.doLayout()
-      // graphInstance.clearChecked()
-      // onCanvasClick()
+    // nextTick(() => {
+    // graphInstance.doLayout()
+    // graphInstance.clearChecked()
+    // onCanvasClick()
+    // });
+  }
+
+  const toggleCropping = (node) => {
+    ElMessageBox.confirm('确定裁剪该节点？', 'Warning', {
+      confirmButtonText: 'OK',
+      cancelButtonText: 'Cancel',
+      type: 'warning'
     })
+      .then(() => {
+        confirmCropping(node)
+      })
+      .catch(() => {
+        ElMessage({
+          type: 'info',
+          message: 'Delete canceled'
+        })
+      })
   }
 
   // 裁剪
-  const toggleCropping = (node) => {
+  const confirmCropping = async (node) => {
     console.log('裁剪', node)
+    loading.value = true
+    // 方案1
+    const newNodes = [...graphData.nodes].filter((n) => n.id != node.id)
+    const newLines = [...graphData.lines].filter(
+      (n) => n.from != node.id && n.to != node.id
+    )
+    const pId = node.lot?.parent?.id
+    const child = node.targetTo
+    for (const c of child) {
+      newLines.push({
+        from: pId,
+        to: c.id,
+        text: '',
+        fontColor: '#333',
+        data: {}
+      })
+    }
+    graphData.nodes = newNodes
+    graphData.lines = newLines
+    await graphInstance.value.setJsonData({ ...graphData })
+
+    // // 方案2
+    // let newRelations: any = [];
+    // const pId = node.lot?.parent?.id;
+    // const child = node.targetTo;
+    // for (const c of child) {
+    //   newRelations.push({
+    //     from: pId,
+    //     to: c.id,
+    //     text: '',
+    //     fontColor: '#333',
+    //     data: {},
+    //   });
+    // }
+    // node.isHide = true;
+    // await graphInstance.value.appendJsonData({ ...graphData, lines: newRelations });
+    loading.value = false
   }
 
   onMounted(() => {
-    showGraph()
+    loading.value = true
+    setTimeout(() => {
+      graphData.rootId = 'c'
+      graphData.nodes = nodes1
+      graphData.lines = lines1
+      showGraph()
+      loading.value = false
+    }, 500)
   })
 </script>
 
@@ -418,7 +494,8 @@
     border: 1px solid #333;
     border-radius: 6px;
     font-size: 24px;
-    position: relative;
+    display: flex;
+    flex-direction: column;
   }
   .title {
     color: #333;
@@ -434,30 +511,24 @@
     border-bottom: #efefef solid 1px;
     background-color: #fff;
     color: #333;
-    padding: 10px 0 40px;
+    padding: 10px 0;
     text-align: center;
-    border-bottom-left-radius: 6px;
-    border-bottom-right-radius: 6px;
     height: 320px;
     box-shadow: 2px 4px 10px #909090;
+    flex: 1;
     div {
       margin: 8px 0;
     }
-    .btn {
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      margin: 0;
-      background-color: #ececec;
-      font-size: 20px;
+  }
+  .btn {
+    background: #f1f1f1;
+    font-size: 20px;
+    padding: 10px 0 5px;
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
-  }
-  .hide-hight {
-    overflow: hidden;
-  }
-  .exp-hight {
-    overflow: auto;
   }
   .text-content {
     border-bottom: #efefef solid 1px;
