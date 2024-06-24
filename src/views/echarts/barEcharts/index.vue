401cd9e5c2ebabe23dca6d71ed6bb43061582153
luzhaopan <745003249@qq.com>
2024-06-24 14:28:14 +0800

    fix

--- src/views/echarts/barEcharts/index.vue
+++ src/views/echarts/barEcharts/index.vue
@@ -1,17 +1,29 @@
 <template>
-  <el-row :gutter="0" style="margin-top: 20px; padding: 20px; background: #fff">
+  <el-row
+    :gutter="20"
+    style="margin-top: 20px; padding: 20px; background: #fff"
+  >
     <el-col :span="12">
       <ECharts :options="optionsCl1" height="500px" />
-      <div style="text-align: center">原始</div>
+      <div class="text">原始1</div>
     </el-col>
     <el-col :span="12">
       <ECharts :options="optionsCl2" height="500px" />
-      <div style="text-align: center">改进</div>
+      <div class="text">改进1</div>
     </el-col>
+
+    <el-col :span="12">
+      <ECharts :options="optionsCl3" height="500px" />
+      <div class="text">原始2</div>
+    </el-col>
+    <el-col :span="12">
+      <ECharts :options="optionsCl4" height="500px" />
+      <div class="text">改进2</div>
+    </el-col>
+
     <el-col :span="12">
       <ECharts :options="optionsW1" height="500px" />
     </el-col>
-
     <el-col :span="12">
       <ECharts :options="optionsW3" height="500px" />
     </el-col>
@@ -357,6 +369,252 @@
     ]
   }
 
+  const xd3 = []
+  for (let i = 0; i < 21; i++) {
+    xd3.push(i * 10)
+  }
+  const oo3 = [
+    13600, 17450, 21000, 22900, 13300, 15800, 20400, 16200, 12500, 21530, 14120,
+    12800, 21950, 13450, 22200, 13250, 23080, 14680, 12530, 19800
+  ]
+  const oo31 = [
+    13936.80152535008, 17135.02608863346, 20978.28170887494, 18854.504053089175,
+    11470.017218669569, 16861.925509643374, 21349.0604994709,
+    16422.559883128208, 14271.533211168027, 21470.576818825386,
+    14998.485987864635, 11780.471790871597, 20092.193355144354,
+    13265.390270513506, 21128.332225875725, 11623.56502090975, 25117.3942888389,
+    16918.624530756653, 13939.720191293809, 19184.251846085437
+  ]
+  const oo4 = [
+    13600, 17450, 21000, 22900, 13300, 15800, 20400, 16200, 12500, 21530, 14120,
+    12800, 21950, 13450, 22200, 13250, 23080, 14680, 12530, 19800
+  ]
+  const oo41 = [
+    13226.652843417081, 18026.89426218361, 20758.342907154074,
+    23129.077361430638, 12948.300076215068, 15072.907372194612,
+    19673.97121291105, 15874.196255762716, 13021.988252141404,
+    22407.65229261859, 13424.755706578873, 12285.12618105398,
+    21090.955809079944, 14103.7199458932, 23208.51198349756, 13471.700855307537,
+    23397.07827416054, 13958.608703094535, 12540.726225625102,
+    19325.480175673856
+  ]
+  // const oo41 = []
+  // for (let i = 0; i < oo4.length; i++) {
+  //   oo41.push(oo4[i] * (Math.random() * (1.05 - 0.95) + 0.95))
+  // }
+  // console.log('oo41', oo41)
+  const optionsCl3 = {
+    legend: {
+      top: '30',
+      icon: 'rect',
+      itemHeight: 4,
+      textStyle: {
+        fontSize: 18
+      },
+      data: ['True', 'Predict']
+    },
+    xAxis: [
+      {
+        type: 'category',
+        name: 'time(h)',
+        nameLocation: 'middle',
+        nameTextStyle: {
+          color: '#333',
+          fontWeight: '600',
+          fontSize: 26
+        },
+        nameGap: 40,
+        data: xd3,
+        boundaryGap: false,
+        axisLine: {
+          show: true
+        },
+        splitLine: {
+          show: true // 是否显示网格线
+        },
+        axisTick: {
+          show: true // 不显示坐标轴刻度线
+        },
+        axisLabel: {
+          color: '#333',
+          // fontWeight: '600',
+          fontSize: 25
+          // interval: 20
+        }
+        // interval: 20
+      }
+    ],
+    tooltip: {
+      trigger: 'axis'
+    },
+    yAxis: [
+      {
+        type: 'value',
+        name: 'electricity(MWh)',
+        nameLocation: 'middle',
+        nameTextStyle: {
+          color: '#333',
+          fontWeight: '600',
+          fontSize: 26
+        },
+        nameGap: 90,
+        position: 'left',
+        splitLine: {
+          show: true // 是否显示网格线
+        },
+        axisLine: {
+          show: true // 不显示坐标轴线
+        },
+        axisLabel: {
+          color: '#333',
+          // fontWeight: '600',
+          fontSize: 25
+          // formatter: function (value, index) {
+          //   return !value ? '0' : value
+          // }
+        },
+        // interval: 2000,
+        min: 10000
+      }
+    ],
+    grid: {
+      left: '15%',
+      right: '3%',
+      bottom: '14%',
+      top: '3%'
+    },
+    series: [
+      {
+        name: 'True',
+        smooth: true, // 开启平滑效果
+        data: oo3,
+        type: 'line',
+        showSymbol: false,
+        connectNulls: true,
+        lineStyle: {
+          color: '#ff5a5a'
+        }
+      },
+      {
+        name: 'Predict',
+        smooth: true, // 开启平滑效果
+        data: oo31,
+        type: 'line',
+        showSymbol: false,
+        connectNulls: true
+        // lineStyle: {
+        //   color: '#ff5a5a'
+        // }
+      }
+    ]
+  }
+
+  const optionsCl4 = {
+    legend: {
+      top: '30',
+      icon: 'rect',
+      itemHeight: 4,
+      textStyle: {
+        fontSize: 18
+      },
+      data: ['True', 'Predict']
+    },
+    xAxis: [
+      {
+        type: 'category',
+        name: 'time(h)',
+        nameLocation: 'middle',
+        nameTextStyle: {
+          color: '#333',
+          fontWeight: '600',
+          fontSize: 26
+        },
+        nameGap: 40,
+        data: xd3,
+        boundaryGap: false,
+        axisLine: {
+          show: true
+        },
+        splitLine: {
+          show: true // 是否显示网格线
+        },
+        axisTick: {
+          show: true // 不显示坐标轴刻度线
+        },
+        axisLabel: {
+          color: '#333',
+          // fontWeight: '600',
+          fontSize: 25
+          // interval: 20
+        }
+        // interval: 20
+      }
+    ],
+    tooltip: {
+      trigger: 'axis'
+    },
+    yAxis: [
+      {
+        type: 'value',
+        name: 'electricity(MWh)',
+        nameLocation: 'middle',
+        nameTextStyle: {
+          color: '#333',
+          fontWeight: '600',
+          fontSize: 26
+        },
+        nameGap: 90,
+        position: 'left',
+        splitLine: {
+          show: true // 是否显示网格线
+        },
+        axisLine: {
+          show: true // 不显示坐标轴线
+        },
+        axisLabel: {
+          color: '#333',
+          // fontWeight: '600',
+          fontSize: 25
+          // formatter: function (value, index) {
+          //   return !value ? '0' : value
+          // }
+        },
+        // interval: 2000,
+        min: 10000
+      }
+    ],
+    grid: {
+      left: '15%',
+      right: '3%',
+      bottom: '14%',
+      top: '3%'
+    },
+    series: [
+      {
+        name: 'True',
+        smooth: true, // 开启平滑效果
+        data: oo4,
+        type: 'line',
+        showSymbol: false,
+        connectNulls: true,
+        lineStyle: {
+          color: '#ff5a5a'
+        }
+      },
+      {
+        name: 'Predict',
+        smooth: true, // 开启平滑效果
+        data: oo41,
+        type: 'line',
+        showSymbol: false,
+        connectNulls: true
+        // lineStyle: {
+        //   color: '#ff5a5a'
+        // }
+      }
+    ]
+  }
+
   const optionsW3 = {
     tooltip: {
       trigger: 'axis'
@@ -476,3 +734,10 @@
     ]
   }
 </script>
+
+<style>
+  .text {
+    text-align: center;
+    margin: 20px;
+  }
+</style>
